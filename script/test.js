f9bc329e721935d1a2bc365a368d0175d06a510c
Keran1205 <a6uzt2vo@students.codeinstitute.net>
2025-03-22 12:18:41 +0000

    Updated search form and changed font style to Montserrat

--- script/test.js
+++ script/test.js
@@ -19,6 +19,61 @@ function validateForm(search) {
   }
 }
 
+/* Form time validation */
+function validateForm(search) {
+  var x = document.forms["search.time"].value;
+  if (x == "") {
+    alert("Time must be filled out");
+    return false;
+  }
+} 
+
+/* Form name validation */
+function validateForm(search) {
+  var x = document.forms["search.name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
+/* Form email validation */ 
+function validateForm(search) {
+  var x = document.forms["search.email"].value;
+  if (x == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+}
+
+/* Form phone validation */
+function validateForm(search) {
+  var x = document.forms["search.phone"].value;
+  if (x == "") {
+    alert("Phone must be filled out");
+    return false;
+  }
+}
+
+/* Form message validation */
+function validateForm(search) {
+  var x = document.forms["search.message"].value;
+  if (x == "") {
+    alert("Message must be filled out");
+    return false;
+  }
+}
+
+/* Form validation */
+function validateForm(search) {
+  var x = document.forms["search"].value;
+  if (x == "") {
+    alert("Form must be filled out");
+    return false;
+  }
+}
+
+
 
 
 
