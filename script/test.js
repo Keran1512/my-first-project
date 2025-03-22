534a9286450d71bd7aa048d1abdcea042f535dc4
Keran1205 <a6uzt2vo@students.codeinstitute.net>
2025-03-22 11:08:48 +0000

    Search form updated to incldue Star rating

--- script/test.js
+++ script/test.js
@@ -1,6 +1,9 @@
+let headingElement = document.getElementById("page-heading");  
+console.log(headingElement);
+
 /* Form destination validation */
-function validateForm() {
-  var x = document.forms["search"]["destination"].value;
+function validateForm(search) {
+  var x = document.forms["search.destination"].value;
   if (x == "") {
     alert("Name must be filled out");
     return false;
@@ -8,8 +11,8 @@ function validateForm() {
 }   
 
 /* Form date validation */
-function validateForm() {
-  var x = document.forms["search"]["date"].value;
+function validateForm(search) {
+  var x = document.forms["search.date"].value;
   if (x == "") {
     alert("Date must be filled out");
     return false;
